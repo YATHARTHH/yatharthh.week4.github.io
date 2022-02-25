afbb153c453f52093bdc20390d48f0ac9cfe2894
Yatharth Singhai <en19cs305048@medicaps.ac.in>
2022-02-25 11:30:40 +0530

    Javascript Project.

--- module4-sol/SpeakGoodBye.js
+++ module4-sol/SpeakGoodBye.js
@@ -1,6 +1,11 @@
+(function(window) {
 
-var speakWord = "Good Bye";
+  var speakWord = "Good Bye";
+  
+    var byeSpeaker = function (name) {
+    console.log(speakWord + " " + name);
+  }
+  
+  window.byeSpeaker = byeSpeaker;
 
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+})(window);
--- module4-sol/SpeakHello.js
+++ module4-sol/SpeakHello.js
@@ -1,6 +1,10 @@
+(function speak(window) {
+  
+  var speakWord = "Hello";
+    var helloSpeaker = function(name){
 
-var speakWord = "Hello";
+    console.log(speakWord + " " + name);
+  }
 
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+  window.helloSpeaker = helloSpeaker;
+})(window);
